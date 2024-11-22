3793535231ebc05ac94763a190e4d53e9d404b3b
bb84729 <bb84729@gmail.com>
2024-11-22 11:14:58 +0800

    add router

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+<style lang="scss">
+@import '@/assets/styles/all.scss'
+</style>
---
+++ layouts/front.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppHeader from '../components/global/AppHeader.vue'
+import AppFooter from '../components/global/AppFooter.vue'
+</script>
+
+
+<template>
+  <div>
+    <AppHeader />
+    <NuxtPage />
+    <AppFooter />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
